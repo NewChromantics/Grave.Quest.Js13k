b9bf586f3aad4bf5cd2cbe4ba1d1ea9881c82a07
Graham Reeves <graham@grahamreeves.com>
2022-08-15 18:59:24 +0100

    Byte counting physics shader. added stick to floor

--- Micro_PhysicsShader.js
+++ Micro_PhysicsShader.js
@@ -1,28 +1,32 @@
 export const Vert =
 `#version 300 es
-vec2 Quad[4] = vec2[4]( vec2(0,0), vec2(1,0), vec2(1,1), vec2(0,1) );
+//	quad crammed into vec4s
+#define u vec4(0,1,1,0)[gl_VertexID]
+#define v vec4(0,0,1,1)[gl_VertexID]
 out vec2 uv;
 void main()
 {
-	uv = Quad[gl_VertexID];
-	gl_Position = vec4( mix( vec2(-1), vec2(1), uv ), 0, 1 );
+	uv = vec2(u,v);
+	gl_Position = vec4( uv*2.0-1.0, 0, 1 );	//	0..1 -> -1..1
 }
 `;
 
 export const Frag =
 `#version 300 es
 precision highp float;
-out vec4 OutFragColor;
+out vec4 Colour;
 in vec2 uv;
 uniform sampler2D OldPositions;
 void main()
 {
-	OutFragColor = vec4(uv,0,1);
+	Colour = vec4(uv,0,1);
 	vec4 xyz = texture(OldPositions, uv);
-	xyz.y -= mix(0.001,0.010,xyz.w);
-	if ( xyz.y < 0.0 )
-		xyz.y += 1.0;
-	OutFragColor = xyz;
+	xyz.y -= mix(0.004,0.015,xyz.w);
+	//	repeat
+	if ( xyz.y < 0.0 )xyz.y += 1.0;
+	//	stick to floor
+	xyz.y = max( xyz.y, 0.45 );
+	Colour = xyz;
 }
 `;
 
